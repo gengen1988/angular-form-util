adf320d52a3642eab3b052e318c8eb5a2805dede
Gen Liu <gg@linux-o4mm.site>
2015-07-09 22:50:29 +0800

    init

---
+++ post.js
@@ -0,0 +1,20 @@
+angular.module('form-util', [])
+  .factory('post', PostFactory);
+
+function PostFactory() {
+  var form = document.createElement('form');
+  form.style.display = 'none';
+  form.method = 'POST';
+  document.body.appendChild(form);
+
+  return function(url, data) {
+    form.action = url;
+    Object.keys(data).forEach(function(key) {
+      var el = document.createElement('input');
+      el.name = key;
+      el.value = data[key];
+      form.appendChild(el);
+    });
+    form.submit();
+  };
+}
